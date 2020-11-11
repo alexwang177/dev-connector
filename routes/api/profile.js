0810fa440a64779a66a49a0f8cb8e97c476d4246
Alex Wang <jianuo.wang@duke.edu>
2020-11-11 18:30:41 -0500

    create and update profile route done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -73,14 +73,42 @@ router.post(
     if (location) profileFields.location = location;
     if (bio) profileFields.bio = bio;
     if (status) profileFields.status = status;
-    if (githubusername) profileFields.username = username;
+    if (githubusername) profileFields.username = githubusername;
     if (skills) {
       profileFields.skills = skills.split(",").map(skill => skill.trim());
     }
 
-    console.log(profileFields.skills);
+    // Build profile social object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
 
-    res.send("Hello");
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        // Update
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      // Create
+      profile = new Profile(profileFields);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
